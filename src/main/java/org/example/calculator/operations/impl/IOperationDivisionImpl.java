3bff97f82a3c3e49885acb56010e9ed274e44bb6
Grishagrishaa <grikha.bomzh78@gmail.com>
2022-12-08 17:43:06 +0300

    fix, 11

--- src/main/java/org/example/calculator/operations/impl/IOperationDivisionImpl.java
+++ src/main/java/org/example/calculator/operations/impl/IOperationDivisionImpl.java
@@ -2,6 +2,8 @@ package org.example.calculator.operations.impl;
 
 import org.example.calculator.operations.api.IOperation;
 
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class IOperationDivisionImpl implements IOperation {
     @Override
     public int execute(double a, double b) {
@@ -9,6 +11,7 @@ public class IOperationDivisionImpl implements IOperation {
         if(result < 0){
             System.out.println("Неположительный результат");
             System.exit(0);
+
         }
         return result;
     }
